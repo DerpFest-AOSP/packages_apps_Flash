statix-leddaz/android_packages_apps_Statix_Flash

Flash: Add some processing settings
Change-Id: I663fd484ba77ad078306797e100f41371619bdc5

app/src/main/java/com/statix/flash/camera/ColorCorrectionAberrationMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+enum class ColorCorrectionAberrationMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/com/statix/flash/camera/DistortionCorrectionMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+enum class DistortionCorrectionMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/com/statix/flash/camera/EdgeMode.kt
@@ -1,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+enum class EdgeMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY,
+    ZERO_SHUTTER_LAG;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency) and
+         * ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+            ZERO_SHUTTER_LAG,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+            ZERO_SHUTTER_LAG,
+        )
+    }
+}

app/src/main/java/com/statix/flash/camera/NoiseReductionMode.kt
@@ -1,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+enum class NoiseReductionMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY,
+    MINIMAL,
+    ZERO_SHUTTER_LAG;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency) and
+         * ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+            ZERO_SHUTTER_LAG,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+            ZERO_SHUTTER_LAG,
+        )
+    }
+}

app/src/main/java/com/statix/flash/camera/ShadingMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+enum class ShadingMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/com/statix/flash/ext/CaptureRequestOptionsBuilder.kt
@@ -8,8 +8,15 @@ package com.statix.flash.ext
 import android.hardware.camera2.CameraMetadata
 import android.hardware.camera2.CaptureRequest
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.camera.camera2.interop.CaptureRequestOptions
+import com.statix.flash.camera.ColorCorrectionAberrationMode
+import com.statix.flash.camera.DistortionCorrectionMode
+import com.statix.flash.camera.EdgeMode
 import com.statix.flash.camera.FrameRate
+import com.statix.flash.camera.HotPixelMode
+import com.statix.flash.camera.NoiseReductionMode
+import com.statix.flash.camera.ShadingMode
 import com.statix.flash.camera.VideoStabilizationMode
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
@@ -36,6 +43,90 @@ fun CaptureRequestOptions.Builder.setVideoStabilizationMode(videoStabilizationMo
                 } else {
                     CameraMetadata.CONTROL_VIDEO_STABILIZATION_MODE_ON
                 }
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setEdgeMode(edgeMode: EdgeMode) {
+    setCaptureRequestOption(
+        CaptureRequest.EDGE_MODE,
+        when (edgeMode) {
+            EdgeMode.OFF -> CameraMetadata.EDGE_MODE_OFF
+            EdgeMode.FAST -> CameraMetadata.EDGE_MODE_FAST
+            EdgeMode.HIGH_QUALITY -> CameraMetadata.EDGE_MODE_HIGH_QUALITY
+            EdgeMode.ZERO_SHUTTER_LAG -> CameraMetadata.EDGE_MODE_ZERO_SHUTTER_LAG
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setNoiseReductionMode(noiseReductionMode: NoiseReductionMode) {
+    setCaptureRequestOption(
+        CaptureRequest.NOISE_REDUCTION_MODE,
+        when (noiseReductionMode) {
+            NoiseReductionMode.OFF -> CameraMetadata.NOISE_REDUCTION_MODE_OFF
+            NoiseReductionMode.FAST -> CameraMetadata.NOISE_REDUCTION_MODE_FAST
+            NoiseReductionMode.HIGH_QUALITY -> CameraMetadata.NOISE_REDUCTION_MODE_HIGH_QUALITY
+            NoiseReductionMode.MINIMAL -> CameraMetadata.NOISE_REDUCTION_MODE_MINIMAL
+            NoiseReductionMode.ZERO_SHUTTER_LAG ->
+                CameraMetadata.NOISE_REDUCTION_MODE_ZERO_SHUTTER_LAG
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setShadingMode(shadingMode: ShadingMode) {
+    setCaptureRequestOption(
+        CaptureRequest.SHADING_MODE,
+        when (shadingMode) {
+            ShadingMode.OFF -> CameraMetadata.SHADING_MODE_OFF
+            ShadingMode.FAST -> CameraMetadata.SHADING_MODE_FAST
+            ShadingMode.HIGH_QUALITY -> CameraMetadata.SHADING_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setColorCorrectionAberrationMode(
+    colorCorrectionAberrationMode: ColorCorrectionAberrationMode
+) {
+    setCaptureRequestOption(
+        CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE,
+        when (colorCorrectionAberrationMode) {
+            ColorCorrectionAberrationMode.OFF -> CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_OFF
+            ColorCorrectionAberrationMode.FAST ->
+                CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_FAST
+            ColorCorrectionAberrationMode.HIGH_QUALITY ->
+                CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.P)
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setDistortionCorrectionMode(
+    distortionCorrectionMode: DistortionCorrectionMode
+) {
+    setCaptureRequestOption(
+        CaptureRequest.DISTORTION_CORRECTION_MODE,
+        when (distortionCorrectionMode) {
+            DistortionCorrectionMode.OFF -> CameraMetadata.DISTORTION_CORRECTION_MODE_OFF
+            DistortionCorrectionMode.FAST -> CameraMetadata.DISTORTION_CORRECTION_MODE_FAST
+            DistortionCorrectionMode.HIGH_QUALITY ->
+                CameraMetadata.DISTORTION_CORRECTION_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setHotPixel(hotPixelMode: HotPixelMode) {
+    setCaptureRequestOption(
+        CaptureRequest.HOT_PIXEL_MODE,
+        when (hotPixelMode) {
+            HotPixelMode.OFF -> CameraMetadata.HOT_PIXEL_MODE_OFF
+            HotPixelMode.FAST -> CameraMetadata.HOT_PIXEL_MODE_FAST
+            HotPixelMode.HIGH_QUALITY -> CameraMetadata.HOT_PIXEL_MODE_HIGH_QUALITY
         }
     )
 }

app/src/main/java/com/statix/flash/ext/SharedPreferences.kt
@@ -14,8 +14,14 @@ import androidx.camera.video.Quality
 import androidx.core.content.edit
 import com.statix.flash.camera.CameraFacing
 import com.statix.flash.camera.CameraMode
+import com.statix.flash.camera.ColorCorrectionAberrationMode
+import com.statix.flash.camera.DistortionCorrectionMode
+import com.statix.flash.camera.EdgeMode
 import com.statix.flash.camera.FlashMode
 import com.statix.flash.camera.FrameRate
+import com.statix.flash.camera.HotPixelMode
+import com.statix.flash.camera.NoiseReductionMode
+import com.statix.flash.camera.ShadingMode
 import com.statix.flash.utils.GestureActions
 import com.statix.flash.utils.GridMode
 import com.statix.flash.utils.TimerMode
@@ -348,4 +354,85 @@ internal val SharedPreferences.volumeButtonsAction: GestureActions
         "nothing" -> GestureActions.NOTHING
         // Default to shutter
         else -> GestureActions.SHUTTER
+    }
+
+// Edge mode
+private const val EDGE_MODE_KEY = "edge_mode"
+private const val EDGE_MODE_DEFAULT = "default"
+internal val SharedPreferences.edgeMode: EdgeMode?
+    get() = when (getString(EDGE_MODE_KEY, EDGE_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> EdgeMode.OFF
+        "fast" -> EdgeMode.FAST
+        "high_quality" -> EdgeMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Noise reduction mode
+private const val NOISE_REDUCTION_MODE_KEY = "noise_reduction_mode"
+private const val NOISE_REDUCTION_MODE_DEFAULT = "default"
+internal val SharedPreferences.noiseReductionMode: NoiseReductionMode?
+    get() = when (getString(NOISE_REDUCTION_MODE_KEY, NOISE_REDUCTION_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> NoiseReductionMode.OFF
+        "fast" -> NoiseReductionMode.FAST
+        "high_quality" -> NoiseReductionMode.HIGH_QUALITY
+        "minimal" -> NoiseReductionMode.MINIMAL
+        // Default to default
+        else -> null
+    }
+
+// Shading mode
+private const val SHADING_MODE_KEY = "shading_mode"
+private const val SHADING_MODE_DEFAULT = "default"
+internal val SharedPreferences.shadingMode: ShadingMode?
+    get() = when (getString(SHADING_MODE_KEY, SHADING_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> ShadingMode.OFF
+        "fast" -> ShadingMode.FAST
+        "high_quality" -> ShadingMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Color correction aberration mode
+private const val COLOR_CORRECTION_ABERRATION_MODE_KEY = "color_correction_aberration_mode"
+private const val COLOR_CORRECTION_ABERRATION_MODE_DEFAULT = "default"
+internal val SharedPreferences.colorCorrectionAberrationMode: ColorCorrectionAberrationMode?
+    get() = when (getString(
+        COLOR_CORRECTION_ABERRATION_MODE_KEY, COLOR_CORRECTION_ABERRATION_MODE_DEFAULT
+    )) {
+        "default" -> null
+        "off" -> ColorCorrectionAberrationMode.OFF
+        "fast" -> ColorCorrectionAberrationMode.FAST
+        "high_quality" -> ColorCorrectionAberrationMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Distortion correction mode
+private const val DISTORTION_CORRECTION_MODE_KEY = "distortion_correction_mode"
+private const val DISTORTION_CORRECTION_MODE_DEFAULT = "default"
+internal val SharedPreferences.distortionCorrectionMode: DistortionCorrectionMode?
+    get() = when (getString(DISTORTION_CORRECTION_MODE_KEY, DISTORTION_CORRECTION_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> DistortionCorrectionMode.OFF
+        "fast" -> DistortionCorrectionMode.FAST
+        "high_quality" -> DistortionCorrectionMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Hot pixel mode
+private const val HOT_PIXEL_MODE_KEY = "hot_pixel_mode"
+private const val HOT_PIXEL_MODE_DEFAULT = "default"
+internal val SharedPreferences.hotPixelMode: HotPixelMode?
+    get() = when (getString(HOT_PIXEL_MODE_KEY, HOT_PIXEL_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> HotPixelMode.OFF
+        "fast" -> HotPixelMode.FAST
+        "high_quality" -> HotPixelMode.HIGH_QUALITY
+        // Default to default
+        else -> null
     }

DerpFest-AOSP/packages_apps_Flash

app/src/main/java/org/derpfest/flash/camera/ColorCorrectionAberrationMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+enum class ColorCorrectionAberrationMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/org/derpfest/flash/camera/DistortionCorrectionMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+enum class DistortionCorrectionMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/org/derpfest/flash/camera/EdgeMode.kt
@@ -1,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+enum class EdgeMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY,
+    ZERO_SHUTTER_LAG;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency) and
+         * ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+            ZERO_SHUTTER_LAG,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+            ZERO_SHUTTER_LAG,
+        )
+    }
+}

app/src/main/java/org/derpfest/flash/camera/NoiseReductionMode.kt
@@ -1,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+enum class NoiseReductionMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY,
+    MINIMAL,
+    ZERO_SHUTTER_LAG;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency) and
+         * ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+            ZERO_SHUTTER_LAG,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+            ZERO_SHUTTER_LAG,
+        )
+    }
+}

app/src/main/java/org/derpfest/flash/camera/ShadingMode.kt
@@ -1,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+enum class ShadingMode {
+    OFF,
+    FAST,
+    HIGH_QUALITY;
+
+    companion object {
+        /**
+         * We don't want to slow down ZSL, when it's enabled allow
+         * only default (the HAL will decide what to do) and OFF (zero latency).
+         */
+        val ALLOWED_MODES_ON_ZSL = setOf(
+            OFF,
+        )
+
+        /**
+         * We don't want to drop frames, when it's enabled allow
+         * only default (the HAL will decide what to do), OFF (zero latency),
+         * FAST (no frame drop guaranteed) and ZERO_SHUTTER_LAG.
+         */
+        val ALLOWED_MODES_ON_VIDEO_MODE = setOf(
+            OFF,
+            FAST,
+        )
+    }
+}

app/src/main/java/org/derpfest/flash/ext/CaptureRequestOptionsBuilder.kt
@@ -8,8 +8,15 @@ package org.derpfest.flash.ext
 import android.hardware.camera2.CameraMetadata
 import android.hardware.camera2.CaptureRequest
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.camera.camera2.interop.CaptureRequestOptions
+import org.derpfest.flash.camera.ColorCorrectionAberrationMode
+import org.derpfest.flash.camera.DistortionCorrectionMode
+import org.derpfest.flash.camera.EdgeMode
 import org.derpfest.flash.camera.FrameRate
+import org.derpfest.flash.camera.HotPixelMode
+import org.derpfest.flash.camera.NoiseReductionMode
+import org.derpfest.flash.camera.ShadingMode
 import org.derpfest.flash.camera.VideoStabilizationMode
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
@@ -36,6 +43,90 @@ fun CaptureRequestOptions.Builder.setVideoStabilizationMode(videoStabilizationMo
                 } else {
                     CameraMetadata.CONTROL_VIDEO_STABILIZATION_MODE_ON
                 }
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setEdgeMode(edgeMode: EdgeMode) {
+    setCaptureRequestOption(
+        CaptureRequest.EDGE_MODE,
+        when (edgeMode) {
+            EdgeMode.OFF -> CameraMetadata.EDGE_MODE_OFF
+            EdgeMode.FAST -> CameraMetadata.EDGE_MODE_FAST
+            EdgeMode.HIGH_QUALITY -> CameraMetadata.EDGE_MODE_HIGH_QUALITY
+            EdgeMode.ZERO_SHUTTER_LAG -> CameraMetadata.EDGE_MODE_ZERO_SHUTTER_LAG
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setNoiseReductionMode(noiseReductionMode: NoiseReductionMode) {
+    setCaptureRequestOption(
+        CaptureRequest.NOISE_REDUCTION_MODE,
+        when (noiseReductionMode) {
+            NoiseReductionMode.OFF -> CameraMetadata.NOISE_REDUCTION_MODE_OFF
+            NoiseReductionMode.FAST -> CameraMetadata.NOISE_REDUCTION_MODE_FAST
+            NoiseReductionMode.HIGH_QUALITY -> CameraMetadata.NOISE_REDUCTION_MODE_HIGH_QUALITY
+            NoiseReductionMode.MINIMAL -> CameraMetadata.NOISE_REDUCTION_MODE_MINIMAL
+            NoiseReductionMode.ZERO_SHUTTER_LAG ->
+                CameraMetadata.NOISE_REDUCTION_MODE_ZERO_SHUTTER_LAG
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setShadingMode(shadingMode: ShadingMode) {
+    setCaptureRequestOption(
+        CaptureRequest.SHADING_MODE,
+        when (shadingMode) {
+            ShadingMode.OFF -> CameraMetadata.SHADING_MODE_OFF
+            ShadingMode.FAST -> CameraMetadata.SHADING_MODE_FAST
+            ShadingMode.HIGH_QUALITY -> CameraMetadata.SHADING_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setColorCorrectionAberrationMode(
+    colorCorrectionAberrationMode: ColorCorrectionAberrationMode
+) {
+    setCaptureRequestOption(
+        CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE,
+        when (colorCorrectionAberrationMode) {
+            ColorCorrectionAberrationMode.OFF -> CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_OFF
+            ColorCorrectionAberrationMode.FAST ->
+                CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_FAST
+            ColorCorrectionAberrationMode.HIGH_QUALITY ->
+                CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.P)
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setDistortionCorrectionMode(
+    distortionCorrectionMode: DistortionCorrectionMode
+) {
+    setCaptureRequestOption(
+        CaptureRequest.DISTORTION_CORRECTION_MODE,
+        when (distortionCorrectionMode) {
+            DistortionCorrectionMode.OFF -> CameraMetadata.DISTORTION_CORRECTION_MODE_OFF
+            DistortionCorrectionMode.FAST -> CameraMetadata.DISTORTION_CORRECTION_MODE_FAST
+            DistortionCorrectionMode.HIGH_QUALITY ->
+                CameraMetadata.DISTORTION_CORRECTION_MODE_HIGH_QUALITY
+        }
+    )
+}
+
+@androidx.camera.camera2.interop.ExperimentalCamera2Interop
+fun CaptureRequestOptions.Builder.setHotPixel(hotPixelMode: HotPixelMode) {
+    setCaptureRequestOption(
+        CaptureRequest.HOT_PIXEL_MODE,
+        when (hotPixelMode) {
+            HotPixelMode.OFF -> CameraMetadata.HOT_PIXEL_MODE_OFF
+            HotPixelMode.FAST -> CameraMetadata.HOT_PIXEL_MODE_FAST
+            HotPixelMode.HIGH_QUALITY -> CameraMetadata.HOT_PIXEL_MODE_HIGH_QUALITY
         }
     )
 }

app/src/main/java/org/derpfest/flash/ext/SharedPreferences.kt
@@ -14,8 +14,14 @@ import androidx.camera.video.Quality
 import androidx.core.content.edit
 import org.derpfest.flash.camera.CameraFacing
 import org.derpfest.flash.camera.CameraMode
+import org.derpfest.flash.camera.ColorCorrectionAberrationMode
+import org.derpfest.flash.camera.DistortionCorrectionMode
+import org.derpfest.flash.camera.EdgeMode
 import org.derpfest.flash.camera.FlashMode
 import org.derpfest.flash.camera.FrameRate
+import org.derpfest.flash.camera.HotPixelMode
+import org.derpfest.flash.camera.NoiseReductionMode
+import org.derpfest.flash.camera.ShadingMode
 import org.derpfest.flash.utils.GestureActions
 import org.derpfest.flash.utils.GridMode
 import org.derpfest.flash.utils.TimerMode
@@ -348,4 +354,85 @@ internal val SharedPreferences.volumeButtonsAction: GestureActions
         "nothing" -> GestureActions.NOTHING
         // Default to shutter
         else -> GestureActions.SHUTTER
+    }
+
+// Edge mode
+private const val EDGE_MODE_KEY = "edge_mode"
+private const val EDGE_MODE_DEFAULT = "default"
+internal val SharedPreferences.edgeMode: EdgeMode?
+    get() = when (getString(EDGE_MODE_KEY, EDGE_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> EdgeMode.OFF
+        "fast" -> EdgeMode.FAST
+        "high_quality" -> EdgeMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Noise reduction mode
+private const val NOISE_REDUCTION_MODE_KEY = "noise_reduction_mode"
+private const val NOISE_REDUCTION_MODE_DEFAULT = "default"
+internal val SharedPreferences.noiseReductionMode: NoiseReductionMode?
+    get() = when (getString(NOISE_REDUCTION_MODE_KEY, NOISE_REDUCTION_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> NoiseReductionMode.OFF
+        "fast" -> NoiseReductionMode.FAST
+        "high_quality" -> NoiseReductionMode.HIGH_QUALITY
+        "minimal" -> NoiseReductionMode.MINIMAL
+        // Default to default
+        else -> null
+    }
+
+// Shading mode
+private const val SHADING_MODE_KEY = "shading_mode"
+private const val SHADING_MODE_DEFAULT = "default"
+internal val SharedPreferences.shadingMode: ShadingMode?
+    get() = when (getString(SHADING_MODE_KEY, SHADING_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> ShadingMode.OFF
+        "fast" -> ShadingMode.FAST
+        "high_quality" -> ShadingMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Color correction aberration mode
+private const val COLOR_CORRECTION_ABERRATION_MODE_KEY = "color_correction_aberration_mode"
+private const val COLOR_CORRECTION_ABERRATION_MODE_DEFAULT = "default"
+internal val SharedPreferences.colorCorrectionAberrationMode: ColorCorrectionAberrationMode?
+    get() = when (getString(
+        COLOR_CORRECTION_ABERRATION_MODE_KEY, COLOR_CORRECTION_ABERRATION_MODE_DEFAULT
+    )) {
+        "default" -> null
+        "off" -> ColorCorrectionAberrationMode.OFF
+        "fast" -> ColorCorrectionAberrationMode.FAST
+        "high_quality" -> ColorCorrectionAberrationMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Distortion correction mode
+private const val DISTORTION_CORRECTION_MODE_KEY = "distortion_correction_mode"
+private const val DISTORTION_CORRECTION_MODE_DEFAULT = "default"
+internal val SharedPreferences.distortionCorrectionMode: DistortionCorrectionMode?
+    get() = when (getString(DISTORTION_CORRECTION_MODE_KEY, DISTORTION_CORRECTION_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> DistortionCorrectionMode.OFF
+        "fast" -> DistortionCorrectionMode.FAST
+        "high_quality" -> DistortionCorrectionMode.HIGH_QUALITY
+        // Default to default
+        else -> null
+    }
+
+// Hot pixel mode
+private const val HOT_PIXEL_MODE_KEY = "hot_pixel_mode"
+private const val HOT_PIXEL_MODE_DEFAULT = "default"
+internal val SharedPreferences.hotPixelMode: HotPixelMode?
+    get() = when (getString(HOT_PIXEL_MODE_KEY, HOT_PIXEL_MODE_DEFAULT)) {
+        "default" -> null
+        "off" -> HotPixelMode.OFF
+        "fast" -> HotPixelMode.FAST
+        "high_quality" -> HotPixelMode.HIGH_QUALITY
+        // Default to default
+        else -> null
     }