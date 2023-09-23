statix-leddaz/android_packages_apps_Statix_Flash

Flash: Add previous() and next() for all enum classes
Change-Id: Ia346055368e9e47e980d453a13e225db35c03b1b

app/src/main/java/com/statix/flash/ext/Enum.kt
@@ -1,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.ext
+
+/**
+ * Get the previous value.
+ */
+internal inline fun <reified E : Enum<E>> E.previous() = enumValues<E>().previous(this)
+
+/**
+ * Get the next value.
+ */
+internal inline fun <reified E : Enum<E>> E.next() = enumValues<E>().next(this)

DerpFest-AOSP/packages_apps_Flash

app/src/main/java/org/derpfest/flash/ext/Enum.kt
@@ -1,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.ext
+
+/**
+ * Get the previous value.
+ */
+internal inline fun <reified E : Enum<E>> E.previous() = enumValues<E>().previous(this)
+
+/**
+ * Get the next value.
+ */
+internal inline fun <reified E : Enum<E>> E.next() = enumValues<E>().next(this)