statix-leddaz/android_packages_apps_Statix_Flash

Flash: Use the view model for views' rotation
Change-Id: I97f5895e182b7d63dc44b2b7cd24ede933d8bf0d

app/src/main/java/com/statix/flash/ui/CapturePreviewLayout.kt
@@ -13,10 +13,12 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.statix.flash.R
+import com.statix.flash.camera.CameraViewModel
 import com.statix.flash.ext.*
 import com.statix.flash.utils.ExifUtils
 import com.statix.flash.utils.MediaType
@@ -46,11 +48,25 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
      */
     internal var onChoiceCallback: (input: Any?) -> Unit = {}
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
+    private val screenRotation
+        get() = cameraViewModel?.screenRotation?.value ?: Rotation.ROTATION_0
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
@@ -133,7 +149,7 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
         }
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val compensationValue = screenRotation.compensationValue.toFloat()
 
         cancelButton.smoothRotate(compensationValue)

app/src/main/java/com/statix/flash/ui/CountDownView.kt
@@ -17,7 +17,9 @@ import android.widget.TextView
 import androidx.annotation.IntRange
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import com.statix.flash.R
+import com.statix.flash.camera.CameraViewModel
 import com.statix.flash.ext.*
 import com.statix.flash.utils.Rotation
 
@@ -49,10 +51,22 @@ class CountDownView(context: Context, attrs: AttributeSet?) : FrameLayout(
     private val isCountingDown: Boolean
         get() = remainingSeconds > 0
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
 
     /**
@@ -125,7 +139,7 @@ class CountDownView(context: Context, attrs: AttributeSet?) : FrameLayout(
         return false
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val compensationValue = screenRotation.compensationValue.toFloat()
 
         remainingSecondsView.smoothRotate(compensationValue)

DerpFest-AOSP/packages_apps_Flash

app/src/main/java/org/derpfest/flash/ui/CapturePreviewLayout.kt
@@ -13,10 +13,12 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import org.derpfest.flash.R
+import org.derpfest.flash.camera.CameraViewModel
 import org.derpfest.flash.ext.*
 import org.derpfest.flash.utils.ExifUtils
 import org.derpfest.flash.utils.MediaType
@@ -46,11 +48,25 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
      */
     internal var onChoiceCallback: (input: Any?) -> Unit = {}
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
+    private val screenRotation
+        get() = cameraViewModel?.screenRotation?.value ?: Rotation.ROTATION_0
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
@@ -133,7 +149,7 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
         }
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val compensationValue = screenRotation.compensationValue.toFloat()
 
         cancelButton.smoothRotate(compensationValue)

app/src/main/java/org/derpfest/flash/ui/CountDownView.kt
@@ -17,7 +17,9 @@ import android.widget.TextView
 import androidx.annotation.IntRange
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import androidx.lifecycle.findViewTreeLifecycleOwner
 import org.derpfest.flash.R
+import org.derpfest.flash.camera.CameraViewModel
 import org.derpfest.flash.ext.*
 import org.derpfest.flash.utils.Rotation
 
@@ -49,10 +51,22 @@ class CountDownView(context: Context, attrs: AttributeSet?) : FrameLayout(
     private val isCountingDown: Boolean
         get() = remainingSeconds > 0
 
-    internal var screenRotation = Rotation.ROTATION_0
+    internal var cameraViewModel: CameraViewModel? = null
         set(value) {
+            val lifecycleOwner = findViewTreeLifecycleOwner() ?: return
+
+            // Unregister
+            field?.screenRotation?.removeObservers(lifecycleOwner)
+
             field = value
-            updateViewsRotation()
+
+            value?.let { cameraViewModel ->
+                cameraViewModel.screenRotation.observe(lifecycleOwner) {
+                    val screenRotation = it ?: return@observe
+
+                    updateViewsRotation(screenRotation)
+                }
+            }
         }
 
     /**
@@ -125,7 +139,7 @@ class CountDownView(context: Context, attrs: AttributeSet?) : FrameLayout(
         return false
     }
 
-    private fun updateViewsRotation() {
+    private fun updateViewsRotation(screenRotation: Rotation) {
         val compensationValue = screenRotation.compensationValue.toFloat()
 
         remainingSecondsView.smoothRotate(compensationValue)