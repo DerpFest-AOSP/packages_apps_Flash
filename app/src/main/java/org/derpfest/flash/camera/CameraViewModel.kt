statix-leddaz/android_packages_apps_Statix_Flash

Flash: Introduce CameraViewModel
Change-Id: I160b10e13be04d6ccdf2b3b623b59ed522e8adbc

app/src/main/java/com/statix/flash/camera/CameraViewModel.kt
@@ -1,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.camera
+
+import androidx.camera.video.Quality
+import androidx.camera.video.Recording
+import androidx.camera.view.video.AudioConfig
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.statix.flash.utils.GridMode
+import com.statix.flash.utils.Rotation
+import com.statix.flash.utils.TimerMode
+
+/**
+ * [ViewModel] representing a camera session. This data is used to receive
+ * live data regarding the setting currently enabled.
+ */
+class CameraViewModel : ViewModel() {
+    // Base
+
+    /**
+     * The camera currently in use.
+     */
+    val camera = MutableLiveData<Camera>()
+
+    /**
+     * Current camera mode.
+     */
+    val cameraMode = MutableLiveData<CameraMode>()
+
+    /**
+     * Whether the current session is in single capture mode.
+     */
+    val inSingleCaptureMode = MutableLiveData(false)
+
+    /**
+     * Current camera state.
+     */
+    val cameraState = MutableLiveData<CameraState>()
+
+    /**
+     * Current screen rotation.
+     */
+    val screenRotation = MutableLiveData<Rotation>()
+
+    // General
+
+    /**
+     * Flash mode.
+     */
+    val flashMode = MutableLiveData(FlashMode.AUTO)
+
+    /**
+     * Grid mode.
+     */
+    val gridMode = MutableLiveData<GridMode>()
+
+    /**
+     * Timer mode.
+     */
+    val timerMode = MutableLiveData<TimerMode>()
+
+    // Photo
+
+    /**
+     * Photo capture mode.
+     */
+    val photoCaptureMode = MutableLiveData<Int>()
+
+    /**
+     * Photo aspect ratio.
+     */
+    val photoAspectRatio = MutableLiveData<Int>()
+
+    /**
+     * Photo effect.
+     */
+    val photoEffect = MutableLiveData<Int>()
+
+    // Video
+
+    /**
+     * Video quality.
+     */
+    val videoQuality = MutableLiveData<Quality>()
+
+    /**
+     * Video frame rate.
+     */
+    val videoFrameRate = MutableLiveData<FrameRate?>()
+
+    /**
+     * Video mic mode.
+     */
+    val videoMicMode = MutableLiveData<Boolean>()
+
+    /**
+     * Video [AudioConfig].
+     */
+    val videoAudioConfig = MutableLiveData<AudioConfig>()
+
+    /**
+     * Video [Recording].
+     */
+    val videoRecording = MutableLiveData<Recording?>()
+}

app/src/main/java/com/statix/flash/ext/MutableLiveData.kt
@@ -1,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.statix.flash.ext
+
+import android.os.Looper
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Set the value immediately if we're in the main thread, else it will post it to be set later.
+ */
+fun <T> MutableLiveData<T>.setOrPostValue(value: T) {
+    if (Looper.getMainLooper().isCurrentThread) {
+        this.value = value
+    } else {
+        postValue(value)
+    }
+}

DerpFest-AOSP/packages_apps_Flash

app/src/main/java/org/derpfest/flash/camera/CameraViewModel.kt
@@ -1,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.camera
+
+import androidx.camera.video.Quality
+import androidx.camera.video.Recording
+import androidx.camera.view.video.AudioConfig
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.derpfest.flash.utils.GridMode
+import org.derpfest.flash.utils.Rotation
+import org.derpfest.flash.utils.TimerMode
+
+/**
+ * [ViewModel] representing a camera session. This data is used to receive
+ * live data regarding the setting currently enabled.
+ */
+class CameraViewModel : ViewModel() {
+    // Base
+
+    /**
+     * The camera currently in use.
+     */
+    val camera = MutableLiveData<Camera>()
+
+    /**
+     * Current camera mode.
+     */
+    val cameraMode = MutableLiveData<CameraMode>()
+
+    /**
+     * Whether the current session is in single capture mode.
+     */
+    val inSingleCaptureMode = MutableLiveData(false)
+
+    /**
+     * Current camera state.
+     */
+    val cameraState = MutableLiveData<CameraState>()
+
+    /**
+     * Current screen rotation.
+     */
+    val screenRotation = MutableLiveData<Rotation>()
+
+    // General
+
+    /**
+     * Flash mode.
+     */
+    val flashMode = MutableLiveData(FlashMode.AUTO)
+
+    /**
+     * Grid mode.
+     */
+    val gridMode = MutableLiveData<GridMode>()
+
+    /**
+     * Timer mode.
+     */
+    val timerMode = MutableLiveData<TimerMode>()
+
+    // Photo
+
+    /**
+     * Photo capture mode.
+     */
+    val photoCaptureMode = MutableLiveData<Int>()
+
+    /**
+     * Photo aspect ratio.
+     */
+    val photoAspectRatio = MutableLiveData<Int>()
+
+    /**
+     * Photo effect.
+     */
+    val photoEffect = MutableLiveData<Int>()
+
+    // Video
+
+    /**
+     * Video quality.
+     */
+    val videoQuality = MutableLiveData<Quality>()
+
+    /**
+     * Video frame rate.
+     */
+    val videoFrameRate = MutableLiveData<FrameRate?>()
+
+    /**
+     * Video mic mode.
+     */
+    val videoMicMode = MutableLiveData<Boolean>()
+
+    /**
+     * Video [AudioConfig].
+     */
+    val videoAudioConfig = MutableLiveData<AudioConfig>()
+
+    /**
+     * Video [Recording].
+     */
+    val videoRecording = MutableLiveData<Recording?>()
+}

app/src/main/java/org/derpfest/flash/ext/MutableLiveData.kt
@@ -1,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.flash.ext
+
+import android.os.Looper
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Set the value immediately if we're in the main thread, else it will post it to be set later.
+ */
+fun <T> MutableLiveData<T>.setOrPostValue(value: T) {
+    if (Looper.getMainLooper().isCurrentThread) {
+        this.value = value
+    } else {
+        postValue(value)
+    }
+}